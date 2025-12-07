0e198d32dc6f764f049d1235c68ab2b4fc18956e
mweiner8 <mweiner8@student.touro.edu>
2025-12-07 18:19:23 -0500

    rebuilding database so Render doesn't have to

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-# Just start Streamlit - database should already be built
+# Just start Streamlit - database is already committed
 streamlit run app.py --server.port="$PORT" --server.address=0.0.0.0 --server.headless=true
